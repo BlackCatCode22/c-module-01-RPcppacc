5af07ffb9b018f9322b73a96f661ba7cf4dfbf85
RPcppacc <repena559@gmail.com>
2025-08-13 11:36:18 -0700

    Add files via upload

---
+++ HelloWorldReview.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World Review!";
+int a = 10;
+    int b = 3;
+    int c = 6;
+    int sum = a + b + c;
+    std::cout << sum << endl;
+    return 0;
+}
