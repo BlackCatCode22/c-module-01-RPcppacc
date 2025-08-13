54481bc02f4aac16891ea1f704b1d26f6a6ee68f
RPcppacc <repena559@gmail.com>
2025-08-13 12:24:32 -0700

    Add files via upload

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,20 @@
+// Created by Roman Pena  on 8/13/2025.
+// create varibales nedde dfor this program
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int sumofnums = 0;
+    int num1 = 0;
+    int num2 = 0;
+    cout << "enter first number: ";
+    num1 = cin.get();
+    cout << "enter second number: ";
+    num2 = cin.get();
+    sumofnums = num1 + num2;
+    cout << "the sum of the first numbers: " << sumofnums << endl;
+
+    return 0;
+}
